7d67b4593de826650ce0904169af19df2be651cf
Zeyad Ahmed <zeyad.ahmed0627@gmail.com>
2026-05-02 02:32:52 +0300

    Enable emulate prepares to catch root PDO exception

--- api/db.php
+++ api/db.php
@@ -20,7 +20,7 @@ try {
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false
+        PDO::ATTR_EMULATE_PREPARES => true
     ]);
     
     // Vercel + Neon PgBouncer connection pooling can sometimes return connections 
